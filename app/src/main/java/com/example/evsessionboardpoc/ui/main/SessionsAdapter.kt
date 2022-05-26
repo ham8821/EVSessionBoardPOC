0a0fc53a9a822595fff3be8bf622659196fe2989
Emma Ham <48266431+ham8821@users.noreply.github.com>
2022-05-27 11:15:13 +1200

    viewholder for session items declared with correct positioning

--- app/src/main/java/com/example/evsessionboardpoc/ui/main/SessionsAdapter.kt
+++ app/src/main/java/com/example/evsessionboardpoc/ui/main/SessionsAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.evsessionboardpoc.ui.main
 
+import android.icu.text.NumberFormat.CURRENCYSTYLE
+import android.util.JsonReader
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.evsessionboardpoc.R
 import com.example.evsessionboardpoc.data.model.Session
 import kotlinx.android.synthetic.main.session_item.view.*
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class SessionsAdapter : RecyclerView.Adapter<SessionsAdapter.SessionViewHolder>(){
 
@@ -23,22 +31,44 @@ class SessionsAdapter : RecyclerView.Adapter<SessionsAdapter.SessionViewHolder>(
 
     override fun getItemCount(): Int = sessions.size
 
-    fun setSessions(movies: List<Session>) {
-        this.sessions = movies
+    fun setSessions(sessions: List<Session>) {
+        this.sessions = sessions
     }
 
-    fun updateSessions(movies: List<Session>) {
-        setSessions(movies)
+    fun updateSessions(sessions: List<Session>) {
+        setSessions(sessions)
         notifyDataSetChanged()
     }
 
     inner class SessionViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val dateView = item.session_item_date
         private val titleView = item.session_item_title
+        private val savings = item.session_item_savings
+        private val cost = item.session_item_cost
+        //create an extension function on a date class which returns a string
+        private fun Date.dateToString(format: String): String {
+            //simple date formatter
+            val dateFormatter = SimpleDateFormat(format, Locale.getDefault())
+            //return the formatted date string
+            return dateFormatter.format(this)
+        }
 
         fun bind(position: Int) {
-            val movie = sessions[position]
+            val session = sessions[position]
 
-            titleView.text = movie.address
+
+            val dateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+
+            val numberFormat = NumberFormat.getCurrencyInstance()
+//            numberFormat.maximumFractionDigits = 0;
+            val formattedSavings = numberFormat.format(session.saving)
+            val formattedCost = numberFormat.format(session.cost)
+
+
+            dateView.text = dateFormatter.parse(session.date).dateToString("dd MMM")
+            titleView.text = session.address
+            savings.text = formattedSavings
+            cost.text = formattedCost
         }
     }
 }
